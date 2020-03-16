57414af0cc5e10d07022772d2ffd5ad7c4f87d0d
Ian K Smith <smithki707@gmail.com>
2020-03-16 11:03:52 -0600

    Remove middlewares module

--- src/admin-sdk/core/sdk.ts
+++ src/admin-sdk/core/sdk.ts
@@ -1,15 +1,9 @@
-import { MiddlewaresModule } from '../modules/middlewares';
 import { TokenModule } from '../modules/token';
 import { UsersModule } from '../modules/users';
 import { MagicAdminSDKAdditionalConfiguration } from '../types';
 
 export class MagicAdminSDK {
   public readonly apiBaseUrl: string;
-
-  /**
-   * Contains token validation middlewares for various NodeJS server frameworks.
-   */
-  public readonly middlewares: MiddlewaresModule;
 
   /**
    * Contains utilities for interacting with Decentralized Identity Tokens
@@ -23,12 +17,11 @@ export class MagicAdminSDK {
    */
   public readonly users: UsersModule;
 
-  constructor(public readonly secretApiKey: string, options?: MagicAdminSDKAdditionalConfiguration) {
-    const endpoint = options?.endpoint ?? 'https://api.fortmatic.com';
+  constructor(public readonly secretApiKey?: string, options?: MagicAdminSDKAdditionalConfiguration) {
+    const endpoint = options?.endpoint ?? 'https://api.magic.link';
     this.apiBaseUrl = endpoint.replace(/\/+$/, '');
 
     // Assign API Modules
-    this.middlewares = new MiddlewaresModule(this);
     this.token = new TokenModule(this);
     this.users = new UsersModule(this);
   }
