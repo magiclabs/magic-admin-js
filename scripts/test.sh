731a8cb440533b459eefc70d29d9f027e1db29fa
Ian K Smith <smithki@users.noreply.github.com>
2020-11-17 11:40:27 -0700

    Cleanup internal scripts (#47)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 echo
-echo "Running unit tests..."
+boxen --border-color cyan --dim-border --padding 1 "🚦 Running unit tests..."
 echo
 
 # Increase memory limit for Node
 export NODE_OPTIONS=--max_old_space_size=4096
 
 export NODE_ENV=test
-
-if [ -n "$1" ]; then
-  input=$(echo $(npx glob $1))
-fi
 
 export TS_NODE_PROJECT="test/tsconfig.json"
 npx nyc --reporter=lcov --reporter=text-summary ava $input
