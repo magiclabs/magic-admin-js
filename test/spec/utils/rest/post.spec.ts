672793d97a9af1b40bddaaf233c83644858db097
Damian Jachyra <44927998+damianjachyra@users.noreply.github.com>
2025-10-10 10:48:39 -0400

    fix: logoutByIssuer

--- test/spec/utils/rest/post.spec.ts
+++ test/spec/utils/rest/post.spec.ts
@@ -26,10 +26,7 @@ test('Successfully POSTs to the given endpoint & stringifies body', async () => 
     'https://example.com/hello/world',
     {
       method: 'POST',
-      headers: { 
-        'X-Magic-Secret-Key': API_KEY,
-        'Content-Type': 'application/json'
-      },
+      headers: { 'X-Magic-Secret-Key': API_KEY },
       body: '{"public_address":"0x0123"}',
     },
   ]);
